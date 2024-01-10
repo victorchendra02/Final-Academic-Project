3163b4ffb02d14eb8e72d702fb480df40778188d
Victor Chendra <victorchendra02@gmail.com>
2024-01-10 11:59:04 +0700

    MINOR

--- labeling_app/index.php
+++ labeling_app/index.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- web icon -->
-    <link rel="icon" type="image/x-icon" href="resources/icons8-geometry-40.png" />
+    <link rel="icon" type="image/x-icon" href="resources/icons8-geometry-40.ico" />
 
     <style>
         * {
