53010dfcf9ea22f2c2f1809ab50a84f7590b60f1
Victor Chendra <victorchendra02@gmail.com>
2024-01-10 11:11:56 +0700

    Edit webicon path

--- labeling_app/index.php
+++ labeling_app/index.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- web icon -->
-    <link rel="icon" type="image/x-icon" href="icons8-geometry-40.png" />
+    <link rel="icon" type="image/x-icon" href="resources/icons8-geometry-40.png" />
 
     <style>
         * {
