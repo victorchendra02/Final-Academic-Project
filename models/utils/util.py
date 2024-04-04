97f3faeedfbc6462072282e2c168a56f2713399b
Victor Chendra <victorchendra02@gmail.com>
2024-04-05 02:12:47 +0700

    Add `save_pkl` & `load_pkl`

--- models/utils/util.py
+++ models/utils/util.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 
 def create_folder(path):
@@ -7,3 +8,46 @@ def create_folder(path):
         print(f"Folder --> '{path}' created")
     except FileExistsError:
         print(f"Already exist --> '{path}'")
+
+
+def save_pkl(obj: any, name: str):
+    """Saves an object to a pickle file, ensuring proper file extension and security.
+
+    Args:
+        obj: The object to serialize and save.
+        name (str): The desired filename or path.
+            - If it doesn't end with ".pkl", `.pkl` will be appended.
+            - Supports relative and absolute paths.
+        protocol (int, optional): The pickle protocol version to use. Defaults to the
+            highest compatible with the current Python version.
+
+    Raises:
+        ValueError: If `name` is an empty string or if file operations fail.
+        PickleError: If pickling encounters an error.
+    """
+
+    
+    if len(name) == 0:
+        raise ValueError("Filename cannot be empty")
+
+    if not name.endswith(".pkl"):
+        name += ".pkl"
+
+    try:
+        with open(name, 'wb') as file:
+            pickle.dump(obj, file)
+    except (OSError, IOError) as e:
+        raise ValueError(f"Failed to save pickle file: {e}") from e
+    except pickle.PickleError as e:
+        raise pickle.PickleError(f"Pickling error: {e}") from e
+
+    print(f"Saved: \"{name}\"")
+
+
+def load_pkl(name: str):
+    if len(name) == 0:
+        raise ValueError("Filename cannot be empty")
+
+    with open(name, 'rb') as file:
+        return pickle.load(file)
+    
