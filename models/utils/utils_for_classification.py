30a1f494e3d90eb1f8f6df8ecb023ebbc4019a50
Victor Chendra <victorchendra02@gmail.com>
2024-04-26 20:24:50 +0700

    Update

--- models/utils/utils_for_classification.py
+++ models/utils/utils_for_classification.py
@@ -1,4 +1,12 @@
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 
 
 def load_text_dataset_from_directory(root_dir: str) -> tuple[list, list]:
@@ -26,42 +34,16 @@ def load_text_dataset_from_directory(root_dir: str) -> tuple[list, list]:
     return X, y
 
 
-def classification_evaluation(y_actual: list, y_pred: list):
-    import numpy as np
-    import matplotlib.pyplot as plt
-    
-    from sklearn.metrics import accuracy_score
-    from sklearn.metrics import precision_score
-    from sklearn.metrics import recall_score
-    from sklearn.metrics import f1_score
-    from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
-
-    l = np.array(["Algebra", "Geometry", "Combinatorics", "Number Theory"])
-    
-    accuracy = accuracy_score(y_actual, y_pred)
-    precision = precision_score(y_actual, y_pred, average='weighted')
-    recall = recall_score(y_actual, y_pred, average='weighted')
-    f1 = f1_score(y_actual, y_pred, average='weighted')
-    
-    print("Overall model accuracy:")
-    print(f"Accuracy  = {accuracy*100:<.2f}%")
-    print(f"Precision = {precision*100:<.2f}%")
-    print(f"Recall    = {recall*100:<.2f}%")
-    print(f"F1-score  = {f1*100:<.2f}%\n")
+def classification_evaluation(y_actual: list, y_pred: list, label: list=["Algebra", "Geometry", "Combinatorics", "Number Theory"]):
+    l = np.array(label)
     
     cm = confusion_matrix(y_actual, y_pred, labels=l)
-
     heatmap = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=l)
     labels = np.unique(y_actual)
-
-    print("Model accuracy for each label:")
-    for label in labels:
-        idx = np.where(l == label)[0][0]
-        label_accuracy = cm[idx, idx] / np.sum(cm[idx, :])
-        print(f"{label:<14s}= {label_accuracy*100:.2f}%")
 
     heatmap.plot()
     plt.show()
     
-    print(classification_report(y_actual, y_pred, digits=4))
+    report  = classification_report(y_actual, y_pred, digits=4, output_dict=True)
+    return report
     
