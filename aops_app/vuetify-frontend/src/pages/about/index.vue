fe6f007680a58813811d92b4198105c9069988c7
Victor Chendra <victorchendra02@gmail.com>
2024-05-14 23:13:15 +0700

    Update about page

--- aops_app/vuetify-frontend/src/pages/about/index.vue
+++ aops_app/vuetify-frontend/src/pages/about/index.vue
@@ -3,7 +3,7 @@
         <v-container class="my-8">
             <h1 class="text-center mb-8">About</h1>
 
-            <v-row class="mb-10">
+            <v-row class="mb-16">
                 <v-col cols="12" md="1"></v-col>
                 <v-col cols="12" md="5">
                     <div class="about-text">
@@ -22,7 +22,7 @@
 
                 <v-col cols="12" md="4">
                     <v-img
-                        src="https://raw.githubusercontent.com/victorchendra02/img-hosting/main/brady-stoeltzing-pk_LRlO-PMk-unsplash.jpg"
+                        src="https://raw.githubusercontent.com/victorchendra02/img-hosting/main/about_1.JPG"
                         max-width="100%"
                         height="auto"
                         rounded="lg"
@@ -31,42 +31,35 @@
                 <v-col cols="12" md="1"></v-col>
             </v-row>
 
+            <br /><br /><br />
+            <h1 class="text-center mb-5">Photos</h1>
+
             <!-- Photo Gallery -->
             <v-row>
-                <v-col cols="6" md="8">
+                <v-col
+                    v-for="photo in photos"
+                    :key="photo.id"
+                    :cols="photo.cols || 6"
+                    :md="photo.md || 4"
+                >
                     <v-img
-                        src="https://raw.githubusercontent.com/victorchendra02/img-hosting/main/kit-fsvhXGARtfQ-unsplash.jpg"
-                        max-width="100%"
-                        height="auto"
+                        :src="`https://raw.githubusercontent.com/${this.username_github}/${this.repo_name}/main/${photo.file_name}`"
+                        aspect-ratio="1"
                         rounded="lg"
-                    ></v-img>
-                </v-col>
-                <v-col cols="6" md="4">
-                    <v-img
-                        src="https://raw.githubusercontent.com/victorchendra02/img-hosting/main/alexander-zaytsev-p3tDpW7UX4w-unsplash.jpg"
-                        max-width="100%"
-                        height="auto"
-                        rounded="lg"
-                    ></v-img>
-                </v-col>
-            </v-row>
-
-            <v-row>
-                <v-col cols="6" md="4">
-                    <v-img
-                        src="https://raw.githubusercontent.com/victorchendra02/img-hosting/main/david-mkrtchian-UGE_Cvtxcs4-unsplash.jpg"
-                        max-width="100%"
-                        height="auto"
-                        rounded="lg"
-                    ></v-img>
-                </v-col>
-                <v-col cols="6" md="8">
-                    <v-img
-                        src="https://raw.githubusercontent.com/victorchendra02/img-hosting/main/manel-sean-U-1LOooJXVc-unsplash.jpg"
-                        max-width="100%"
-                        height="auto"
-                        rounded="lg"
-                    ></v-img>
+                    >
+                        <template v-slot:placeholder>
+                            <v-row
+                                class="fill-height ma-0"
+                                align="center"
+                                justify="center"
+                            >
+                                <v-progress-circular
+                                    indeterminate
+                                    color="grey lighten-5"
+                                ></v-progress-circular>
+                            </v-row>
+                        </template>
+                    </v-img>
                 </v-col>
             </v-row>
         </v-container>
@@ -75,7 +68,46 @@
 
 <script>
 export default {
-    name: "AboutMePage",
+    data: () => ({
+        username_github: "victorchendra02",
+        repo_name: "img-hosting",
+        photos: [
+            { id: 1, file_name: "about_1.JPG" },
+            { id: 2, file_name: "about_2.JPG" },
+            { id: 3, file_name: "about_3.jpg" },
+            { id: 4, file_name: "about_4.jpg" },
+            { id: 5, file_name: "about_5.jpg" },
+            { id: 6, file_name: "about_6.JPG" },
+            { id: 7, file_name: "about_7.JPG" },
+            { id: 8, file_name: "about_8.JPG" },
+            { id: 9, file_name: "about_9.jpg" },
+            { id: 10, file_name: "about_10.JPG" },
+            { id: 11, file_name: "about_11.jpg" },
+            { id: 12, file_name: "about_12.jpg" },
+            { id: 13, file_name: "about_13.jpg" },
+            { id: 14, file_name: "about_14.jpg" },
+            { id: 15, file_name: "about_15.jpg" },
+            { id: 16, file_name: "about_16.jpg" },
+            { id: 17, file_name: "about_17.jpg" },
+            { id: 18, file_name: "about_18.jpg" },
+        ],
+    }),
+
+    methods: {
+        shufflePhotos() {
+            for (let i = this.photos.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [this.photos[i], this.photos[j]] = [
+                    this.photos[j],
+                    this.photos[i],
+                ];
+            }
+        },
+    },
+
+    mounted() {
+        this.shufflePhotos();
+    },
 };
 </script>
 
